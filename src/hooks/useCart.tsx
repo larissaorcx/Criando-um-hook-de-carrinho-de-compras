d11738f0e201982c14eb6bb1de149b9badcf3d44
Ingrid Larissa <larissaorcx@gmail.com>
2021-10-27 20:07:55 -0300

    Alinhando a aplicação aos testes

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -35,20 +35,20 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const addProduct = async (productId: number) => {
     try {
 
-      const stockProduct = await api.get(`/stock/${productId}`)
+      const stockProduct = await api.get<Stock>(`/stock/${productId}`)
       .then(response => response.data.amount)
       
       const newCart = [...cart]
 
       const sameIndex = newCart.findIndex(indice => indice.id === productId)
-      
 
       if(sameIndex !== -1){
         if (newCart[sameIndex].amount >= stockProduct){
           toast.error('Quantidade solicitada fora de estoque');
+          return
         }
         else{
-          newCart[sameIndex].amount  += 1
+          newCart[sameIndex].amount += 1
         }
       }else{
         const initialProduct = await api.get(`/products/${productId}`);
@@ -76,13 +76,15 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
       if(trash !== -1){
         removeCart.splice(trash,1)
-
-        console.log('removido', trash)
+      }else{
+        toast.error('Erro na remoção do produto');
+        return
       }
-    
+      
       setCart(removeCart)
-
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(removeCart));
+  
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(removeCart)); 
+      
     } catch {
       toast.error('Erro na remoção do produto');
     }
@@ -93,8 +95,10 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      console.log("entrou")
-
+      
+      if(amount <= 0){
+        return
+      }
       const updateAmount = [...cart]
 
       const productAmount = await api.get(`/stock/${productId}`)
@@ -103,22 +107,12 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       const productIndex = updateAmount.findIndex(indice => indice.id === productId)
 
       if(productIndex !== -1){
-        if(updateAmount[productIndex].amount <= 0){
+        
+        if (amount <= productAmount){
+          updateAmount[productIndex].amount = amount
+        }else{
+          toast.error('Quantidade solicitada fora de estoque');
           return
-        }
-        if(amount === 1){
-          if ((updateAmount[productIndex].amount) < productAmount){
-            updateAmount[productIndex].amount += amount
-          }else{
-            toast.error('Quantidade solicitada fora de estoque');
-          }
-        }
-        if(amount === -1){
-          if ((updateAmount[productIndex].amount) <= productAmount){
-            updateAmount[productIndex].amount += amount
-          }else{
-            toast.error('Quantidade solicitada fora de estoque');
-          }
         }
       }
 
